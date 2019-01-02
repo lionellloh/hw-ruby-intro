736c5dd7a1b58206624a48324b04be63cf2e2230
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2019-01-02 09:14:54 +0800

    part 1 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -24,31 +24,57 @@ def max_2_sum arr
     biggest = arr[1]
     next_biggest = arr[0]
   end
-
   arr[2,arr.length].each_entry do |i|
     if i > biggest
-      biggest = next_biggest
+      biggest = i
     elsif i > next_biggest
       next_biggest = i
     end
   end
   biggest + next_biggest
 end
-p max_2_sum(input_arr)
+p max_2_sum([1, 2, 3, 3])
 
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  hash = {}
+  # You can't have 2 integers if the arr.length is <=1
+  if arr.length <=1
+    return false
+  end
+  # Initialize: put everything into a hash
+  arr.each do |i|
+    if hash.key?(i)
+      hash[i] +=1
+    else
+      hash[i] =1
+    end
+  end
+
+  arr.each do |i|
+    complement = n - i
+    if hash.key?(complement)
+      if complement == i
+        return hash[complement] > 1
+      end
+      return true
+    end
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  vowels = %w(a e i o u)
+  if vowels.include?(s[0])
+    return false
+  end
+  true
 end
 
 def binary_multiple_of_4? s
